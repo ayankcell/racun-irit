5264aff7864febd7ca70206b0977b78643fe444f
ayankcell <a.ahyarudin@gmail.com>
2022-03-26 06:13:54 +0700

    change WP host

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-    baseHost : 'https://public-api.wordpress.com/rest/v1.1/sites/sadiskon.wordpress.com',
+    baseHost : 'https://public-api.wordpress.com/rest/v1.1/sites/racunproduk.wordpress.com',
     perPage: 12,
     monthString: new Date().toLocaleDateString("id-ID", {month: "long",year: "numeric"})
 })
