c3be16e34be5c82c38224bf2afe8434dae3cdc2f
ayankcell <a.ahyarudin@gmail.com>
2022-03-29 19:41:34 +0700

    upsize perpage results

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
     baseHost : 'https://public-api.wordpress.com/rest/v1.1/sites/racunproduk.wordpress.com',
-    perPage: 12,
-    monthString: new Date().toLocaleDateString("id-ID", {month: "long",year: "numeric"})
+    perPage: 18,
+    // monthString: new Date().toLocaleDateString("id-ID", {month: "long",year: "numeric"})
 })
