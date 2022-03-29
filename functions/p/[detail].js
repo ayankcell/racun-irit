5196057d9fd42e2b58381095d4250499a413dfb6
ayankcell <a.ahyarudin@gmail.com>
2022-03-29 09:25:19 +0700

    return params

--- functions/p/[detail].js
+++ functions/p/[detail].js
@@ -9,5 +9,5 @@ export async function onRequest(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
   
-    return new Response(params);
+    return new Response(params.detail);
   }
