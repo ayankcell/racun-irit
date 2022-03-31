5f25d0ddb24382586b653198d209793cb017e42b
ayankcell <a.ahyarudin@gmail.com>
2022-03-31 18:55:50 +0700

    Sementara Live

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
     baseHost : 'https://iritlink.hack.id/wp-json/wp/v2',
-    perPage: 2,
+    perPage: 18,
     // monthString: new Date().toLocaleDateString("id-ID", {month: "long",year: "numeric"})
 })
