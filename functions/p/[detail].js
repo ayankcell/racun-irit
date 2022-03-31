552af89bcc91f3fe1bde960a6ad5ab1f3540a6c4
ayankcell <a.ahyarudin@gmail.com>
2022-03-31 20:31:21 +0700

    coba ke self lg1

--- functions/p/[detail].js
+++ functions/p/[detail].js
@@ -19,7 +19,7 @@ export async function onRequest(context) {
       //template(racun[0]).replace(/[^\S\r\n]+/g,' ')
       
 
-      return new Response( response.type , { // replace new lines juga
+      return new Response( response.statusText + ' '+response.url , { // replace new lines juga
          headers: {
             'content-type': 'text/plain;charset=UTF-8'
          }
