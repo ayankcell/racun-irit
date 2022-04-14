08bf431053046c4dbb842a9326fd2085a1415111
ayankcell <a.ahyarudin@gmail.com>
2022-04-14 16:58:56 +0700

    replace racun.irit.link in functions

--- functions/p/[detail].js
+++ functions/p/[detail].js
@@ -72,7 +72,7 @@ export const template = (racun, merchant ) => {
       <meta name="twitter:image" content="${racun.featured_image}">
       <meta name="twitter:card"content="summary">
       <meta name="facebook-domain-verification" content="0l7ou7u0f89oasssyjl0k54qdb3oq0" />
-      <link rel="canonical" href="https://racun.irit.link/p/${racun.slug}/">
+      <link rel="canonical" href="https://irit.link/p/${racun.slug}/">
       <script src="/assets/js/umbrella.js" defer></script>
       <script src="/assets/js/clipboard.min.js" defer></script>
    </head>
@@ -168,7 +168,7 @@ export const template = (racun, merchant ) => {
                            </span>
                            <span class="text-white">Twitter</span>
                         </a>
-                        <button class="racun-social_copylink flex w-full p-2 justify-center item-center gap-2 my-2 rounded-xl bg-gray-700" data-clipboard-text="Aku nemuin promo - ${racun.title} https://racun.irit.link/${racun.slug}">
+                        <button class="racun-social_copylink flex w-full p-2 justify-center item-center gap-2 my-2 rounded-xl bg-gray-700" data-clipboard-text="Aku nemuin promo - ${racun.title} https://irit.link/${racun.slug}">
                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="h-5 w-5">
                               <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd"></path>
                            </svg>
@@ -223,7 +223,7 @@ export const template = (racun, merchant ) => {
     }) //  document.addEventListener("DOMContentLoaded")
    
    function sharePopup(network) {
-      var url = 'http://racun.irit.link/p/${racun.slug}/'
+      var url = 'http://irit.link/p/${racun.slug}/'
       var title = '${racun.title}'
       var excerpt = '${encodeURIComponent(racun.excerpt.replace(/(<([^>]+)>)/ig, ''))}'
       var msg = encodeURIComponent('Aku nemu ini -> ') + title + excerpt
