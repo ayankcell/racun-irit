be9798057fc7449e80b066e890b66dfd92887675
ayankcell <a.ahyarudin@gmail.com>
2022-08-17 21:49:37 +0700

    fb domain verif

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {name :'facebook-domain-verification', content: '66z0e2yoz3uw7o0pi3uyzjam0myme2'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
