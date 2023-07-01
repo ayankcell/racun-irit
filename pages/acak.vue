44caedc2e79e50e7055450c2741dc13215b6a346
Akhyar <a.ahyarudin@gmail.com>
2023-07-01 10:20:40 +0700

    Update acak.vue

--- pages/acak.vue
+++ pages/acak.vue
@@ -2,14 +2,14 @@
   <div
     class="max-w-screen-sm mx-auto bg-white h-screen flex flex-col justify-center items-center"
   >
-    <h1 class="text-xl font-semibold py-3 my-4">Acak Angka</h1>
+    <h1 class="text-xl font-semibold py-3 my-4">Undi Extra Bonus Rahasia OCLP</h1>
 
     <button
       @click="putar()"
       class="w-[300px] h-[300px] rounded-full bg-yellow-200 p-5"
     >
       <div class="w-full h-full flex flex-col justify-center items-center bg-yellow-300 rounded-full">
-        <span v-if="ranNum" class="text-sm">Angka Kamu:</span>
+        <span v-if="ranNum" class="text-sm">Yang Beruntung:</span>
         <span v-if="ranNum" class="text-6xl font-semibold text-blue-700">
           {{ ranNum }}
         </span>
