06d6a25478edf4f64cb6ee737e8987d6738bd2d3
ayankcell <ayankcell@gmail.com>
2024-12-12 09:34:57 +0700

    feat: error message

--- pages/[redir].vue
+++ pages/[redir].vue
@@ -8,6 +8,12 @@ const path = route.params.redir
 
 /** get long url from path / slug */
 const { data } = await useAsyncData('redirecton', () => queryContent(path).findOne())
+
+/** error 404 when no valid redirect path */
+if (!data.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+}
+
 const targetUrl = data.value?.targetUrl +'?utm_source=irit.link&utm_medium=referral'
 
 navigateTo(
